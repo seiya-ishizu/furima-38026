4ddfa2c7808f03af379a599e3423a558b6d80a8b
seiya-ishizu <kk4jt5vrv@icloud.com>
2022-06-22 17:17:04 +0900

    syntaxerror

--- db/migrate/20220618082814_create_shipping_addresses.rb
+++ db/migrate/20220618082814_create_shipping_addresses.rb
@@ -5,7 +5,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[6.0]
       t.integer      :prefecture_id,    null: false
       t.string       :city,             null: false
       t.string       :block,            null: false
-      t.string       :building,        
+      t.string       :building
       t.string       :phone_number,     null: false
       t.references   :purchase_history, null: false, foreign_key: true
       t.timestamps
